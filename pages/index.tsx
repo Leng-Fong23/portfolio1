9ebc0d03f3c25a4684bdfcbed4255a5d150d12e7
lengfong <lengfong2303@gmail.com>
2023-12-21 16:41:53 +0800

    Fixed stuff

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,17 +68,17 @@ const Home: NextPage = () => {
     
           <div className={styles.gradient}>
             <div className={styles.img}>
-            <Image src="/LF.png" alt="lengfong" width="500" height='500' className={styles.img}/>
+            <img src="/LF.png" alt="lengfong" width="500" height='500' className={styles.img}/>
           </div>
             </div>
 
         <div id= "home" className={styles.box}>
-        <p className={styles.text}>
+        <div className={styles.text}>
           <h1 className={styles.h1}>About Myself</h1>
-        </p>
+        </div>
           <div className={styles.content}>
               <div className={styles.imga}>
-                  <Image src="/Image.jpeg" alt="lengfong" width="200" height='100' className={styles.img}/> <br />
+                  <img src="/Image.jpeg" alt="lengfong" width="200" height='100' className={styles.img}/> <br />
               </div>
               <div className={styles.text}>
                   <p>
@@ -89,7 +89,7 @@ const Home: NextPage = () => {
         </div>
         
         <div id= "skill" className={styles.boxa}>
-        <p className={styles.text}>
+        <div className={styles.text}>
         <h1 className={styles.h1}>Skill</h1>
         <div className={styles.contenta}>
           •	Familiar with different operating system (OSes) like Windows, MacOS, and Linux. <br />
@@ -100,7 +100,7 @@ const Home: NextPage = () => {
           •	Skilled in HTML, CSS and JavaScript.<br />
           •	Ability to communicate with different group of people, lead a team, follow instructions, and get a job done on time.<br />
           </div>
-        </p>
+        </div>
         </div>
        
         <div id= "achivement" className={styles.box}>
@@ -136,25 +136,21 @@ const Home: NextPage = () => {
         <h1 className={styles.h1}>Education</h1>
               <div className={styles.contenta}>
                 <h2 className={styles.year}>August 2021 to present</h2>
-                <p>
                 BSc (Hons) Information Technology (Computer Networking and Security) <br />
                 Sunway University, Selangor <br />
                 Expected to graduate in August 2024 <br />
                 Major: Computer Networking and Security <br />
                 CGPA of Year 1: 3.57 <br />
                 CGPA until latest semester: 3.65 <br />
-                </p>
               </div>
             <div className={styles.containerright}>
               <div className={styles.contenta}>
                 <h2 className={styles.year}>September 2020 to July 2021</h2>
-                <p>
                 July 2021 <br />
                 Foundation In Science and Technology <br />
                 Sunway College, Selangor <br />
                 Course taken: Introduction to Programming, Basic Computer Concept, ICT Application Skills <br />
                 Results: 3.84 CGPA <br />
-                </p>
               </div>
             </div>
             </div>
@@ -165,69 +161,55 @@ const Home: NextPage = () => {
         <h1 className={styles.h1}>Extracurricular activities</h1>
               <div className={styles.content}>
                 <h2 className={styles.year}>2023</h2>
-                <p>
                <p className={styles.topic}> Member of Sunway Student Volunteer <br /></p> 
               •	Join activities that have been organized by Sunway Student Volunteer
-                </p>
               </div>
             <div className={styles.containerright}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2022</h2>
-                <p>
                 <p className={styles.topic}>  Vice Head of Department of Program & Committee Member of 30 Hours Famine in Sunway University.<br /></p> 
                 •	Organize a DIY camp for 30 hours famine.<br />
                 •	Communicate with other committee members and give ideas for the event.<br />
-                </p>
               </div>
             </div>
             <div className={styles.containerleft}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2016 to 2019</h2>
-                <p>
                 <p className={styles.topic}> Sin Chew Student Reporter Cadet <br /></p> 
                   •	Organized 2 major camps. <br />
                   •	Learned how to work together in a huge team. <br />
-                </p>
               </div>
             </div>
             <div className={styles.containerright}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2015-2019</h2>
-                <p>
                 <p className={styles.topic}> Volunteer of Buddhist Society, Kelantan.<br /></p> 
                 •	Organized Buddhist camp for kids below 12 years old for thrice.<br />
                 •	Organized talent competition and master of the ceremonies of the competition.<br />
                 •	Organized Mid-autumn carnivals.<br />
-                </p>
               </div>
             </div>
             <div className={styles.containerleft}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2015-2019</h2>
-                <p>
                 <p className={styles.topic}> President of Basketball Team & Committee Member, SMK Dato Mahmmud Paduka Raja (1)<br /></p> 
                 •	Guided all the members in basketball team.<br />
                 •	Responsible in running club meetings, appointing committee, and delegating tasks<br />
-                </p>
               </div>
             </div>
             <div className={styles.containerright}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2015-2019</h2>
-                <p>
                 <p className={styles.topic}> Treasurer of Chinese Society Club & Committee Member, SMK Dato Mahmmud Paduka Raja (1)<br /></p> 
                 •	Oversaw the financial administration of the Chinese Society Club.<br />
                 •	As a photographer for all activities and the person who managed Facebook page of the club.<br />
-                </p>
               </div>
             </div>
             <div className={styles.containerright}>
               <div className={styles.content}>
                 <h2 className={styles.year}>2015-2019</h2>
-                <p>
                 <p className={styles.topic}>  Committee Member of Red Crescent, SMK Dato Mahmmud Paduka Raja (1)<br /></p> 
                 •	Volunteer of rescue team for annual sport in Dato Mahmmud and SJK  (C) Yuk Cheng.<br />
-                </p>
               </div>
             </div>
             </div>
