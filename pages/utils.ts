6c95186324b7a7d33f1be9210f818e84a4c84d28
lengfong <lengfong2303@gmail.com>
2024-01-30 19:58:43 +0800

    fixed stuff

--- pages/utils.ts
+++
@@ -1,8 +0,0 @@
-// utils.ts
-export const handleNavClick = (link: string) => {
-    const section = document.getElementById(link.substring(1));
-    if (section) {
-      section.scrollIntoView({ behavior: 'smooth' });
-    }
-  };
-  
